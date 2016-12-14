19708e55798c1087ed98c32fe37b3ae4445b93b1
ITユーザー <it-user@kpc-it3161mc.kmt.neec.ac.jp>
2016-12-14 14:41:22 +0900

    #3 end

--- ex1-1-3/ex1-1-3/ViewController.m
+++ ex1-1-3/ex1-1-3/ViewController.m
@@ -17,6 +17,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSInteger num = 100;
+    NSArray *color = @[@"red", @"blue", @"green"];
+    
+    if(num == 100) {
+        NSLog(@"%@", @"Yes");
+    }
+    
+    if(num == 1000) {
+        NSLog(@"%@", @"Yes");
+    } else {
+        NSLog(@"%@", @"No");
+    }
+    
+    if(num > 100) {
+        NSLog(@"%@", @"Over 100");
+    } else if(num < 100) {
+        NSLog(@"%@", @"under 100");
+    } else {
+        NSLog(@"%@", @"100");
+    }
+    
+    NSLog(@"%@", (num == 100)?@"Yes":@"No");
+    
+    for(int i = 0; i < 10; i++) {
+        NSLog(@"%ld", (long)i);
+    }
+    
+    for(NSString *str in color) {
+        NSLog(@"%@", str);
+    }
+    
+    switch (num % 2) {
+        case 1:
+            NSLog(@"%@", @"Odd number");
+            break;
+        case 0:
+            NSLog(@"%@", @"even number");
+            break;
+            
+        default:
+            break;
+    }
 }
 
 - (void)didReceiveMemoryWarning {

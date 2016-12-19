01cd552c325bc17b7390d00842027e8c96d52cad
ITユーザー <it-user@kpc-it3161mc.kmt.neec.ac.jp>
2016-12-19 11:33:52 +0900

    #4 start

---
+++ ex1-1-4/ex1-1-4/Account.h
@@ -0,0 +1,13 @@
+//
+//  Account.h
+//  ex1-1-4
+//
+//  Created by ITユーザー on 2016/12/19.
+//  Copyright © 2016年 ITユーザー. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Account : UIViewController
+
+@end
---
+++ ex1-1-4/ex1-1-4/Account.m
@@ -0,0 +1,15 @@
+//
+//  Account.m
+//  ex1-1-4
+//
+//  Created by ITユーザー on 2016/12/19.
+//  Copyright © 2016年 ITユーザー. All rights reserved.
+//
+
+#import "Account.h"
+
+@implementation Account
+
++(void)profile;
+
+@end
